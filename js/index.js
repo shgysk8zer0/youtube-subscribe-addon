bed74e048129420a61e615bd437bc426f2408ccf
Chris Zuber <shgysk8zer0@gmail.com>
2017-11-13 21:04:31 -0800

    Basic functional ext
    - Supports feeds for channels & playlists
    - Need users, if YouTube supports this
    - Remove all unnecessary files
    - Create custom icon
    - Remove translations (were for different project)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-// Do stuff
+async function scanTab(tab) {
+	const url = new URL(tab.url);
+
+	if (url.pathname.startsWith('/channel/') || url.searchParams.has('list')) {
+		browser.pageAction.show(tab.id || tab.tabId);
+		browser.pageAction.onClicked.addListener(clickHandler);
+	}
+}
+
+async function clickHandler(tab) {
+	const url = new URL(tab.url);
+	const feedUrl = new URL('/feeds/videos.xml', url.origin);
+
+	try {
+		if (url.pathname.startsWith('/channel/')) {
+			const channelId = url.pathname.split('/')[2];
+			feedUrl.searchParams.set('channel_id', channelId);
+		} else if (url.searchParams.has('list')) {
+			feedUrl.searchParams.set('playlist_id', url.searchParams.get('list'));
+		} else {
+			throw new Error(`There is no feed for ${url.toString()}`);
+		}
+
+		browser.tabs.create({
+			url: feedUrl.toString(),
+		});
+	} catch(err) {
+		console.error(err);
+	}
+}
+
+async function refreshAllTabsPageAction() {
+	const tabs = await browser.tabs.query({});
+	tabs.forEach(scanTab);
+}
+
+browser.tabs.onUpdated.addListener(refreshAllTabsPageAction);
+browser.runtime.onInstalled.addListener(refreshAllTabsPageAction);
